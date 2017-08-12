7be7f184daf42e1626184b6bfb65626c5191a648
gopisellamuthu <30342205+gopisellamuthu@users.noreply.github.com>
2017-08-12 06:44:05 +0530

    Update 8.c

--- 8.c
+++ 8.c
@@ -1,18 +1,15 @@
-#include <stdio.h>
-int main()
-{
-    int n, i, sum = 0;
-    
-    printf("Enter a positive integer: ");
-    scanf("%d",&n);
-
-    for(i=1; i <= n; ++i)
-    {
-        sum += i;   
-    }
-
-    printf("Sum = %d",sum);
-
-    return 0;
-}
-
+2: #include<stdio.h>
+3:  int main()
+      {
+5:     int i, sum = 0; 
+6:     int n;
+7:     printf("Enter the number: ");
+8:     scanf("%d",&n);
+9: 
+10:     for(i = 0; i <=n; i++){
+11:         sum = sum + i;
+12:     }
+13: 
+14:     printf("The sum of first %d natural number is %d\n\n", n,sum);
+15:     return 0;
+16: }

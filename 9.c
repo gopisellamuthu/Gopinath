5f61a2fb8e0924ddfc44ab4f6a9dd811147c65fe
gopisellamuthu <30342205+gopisellamuthu@users.noreply.github.com>
2017-08-12 06:48:37 +0530

    Update 9.c

--- 9.c
+++ 9.c
@@ -1,17 +1,16 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-    int n, i, sum = 0;
-    
-    printf("Enter a positive integer: ");
-    scanf("%d",&n);
-
-    for(i=1; i <= n; ++i)
-    {
-        sum += i;   // sum = sum+i;
-    }
-
-    printf("Sum = %d",sum);
-
-    return 0;
+  int Number, i, Sum = 0;
+  
+  printf("\nPlease Enter any Integer Value\n");
+  scanf("%d", &Number);
+  
+  for(i = 1; i <= Number; i++)
+  {
+     Sum = Sum + i;
+  }
+  
+  printf("Sum of Natural Numbers = %d", Sum);
+  return 0;
 }

ae3e5de50b7af5824797798a2c964142dfb0f800
gopisellamuthu <30342205+gopisellamuthu@users.noreply.github.com>
2017-08-14 09:54:28 +0530

    Update 11.c

--- 11.c
+++ 11.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include <conio.h>
- 
-int main(){
+  
+int main()
+{
     int base, exponent, counter, result = 1;
     printf("Enter base and exponent \n");
     scanf("%d %d", &base, &exponent);
-    for(counter = 0; counter < exponent; counter++){
-        result = result * base;
-    }
-     
+    
+    result = getPower(base, exponent);
+    
     printf("%d^%d = %d", base, exponent, result);
     getch();
     return 0;
+}
+
+int getPower(int base, int exponent)
+{
+    if(exponent == 0)
+    {
+        return 1;
+    }
+    return base * getPower(base, exponent - 1);
 }

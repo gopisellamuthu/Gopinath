fd4e1e778a4fbfc363f3a3f5977aefb8d2b60d73
gopisellamuthu <30342205+gopisellamuthu@users.noreply.github.com>
2017-08-14 12:59:32 +0530

    Update 20.c

--- 20.c
+++ 20.c
@@ -1,18 +1,12 @@
-#include <stdio.h>
-int main()
-{
-    int n, i, range;
-
-    printf("Enter an integer: ");
-    scanf("%d",&n);
-
-    printf("Enter the range: ");
-    scanf("%d", &range);
-
-    for(i=1; i <= range; ++i)
-    {
-        printf("%d * %d = %d \n", n, i, n*i);
-    }
-
-    return 0;
+#include<stdio.h>
+int main(){
+  int r,i,j,k;
+  printf("Enter the number range: ");
+  scanf("%d",&r);
+  for(i=1;i<=r;i++){
+      for(j=1;j<=10;j++)
+           printf("%d*%d=%d ",i,j,i*j);
+      printf("\n");
+  }
+  return 0;
 }

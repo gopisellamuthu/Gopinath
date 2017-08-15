9f49e8a89f09242ce41b9036444f803db65233b4
gopisellamuthu <30342205+gopisellamuthu@users.noreply.github.com>
2017-08-15 09:37:06 +0530

    Update 22.c

--- 22.c
+++ 22.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
-int gcd(int a, int b)
-{
-    if (a == 0 || b == 0)
-       return 0;
-    if (a == b)
-        return a;
-    if (a > b)
-        return gcd(a-b, b);
-    return gcd(a, b-a);
-}
 int main()
 {
-    int a = 98, b = 56;
-    printf("GCD of %d and %d is %d ", a, b, gcd(a, b));
+    int n1, n2, i, gcd;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &n1, &n2);
+
+    for(i=1; i <= n1 && i <= n2; ++i)
+    {
+      
+        if(n1%i==0 && n2%i==0)
+            gcd = i;
+    }
+
+    printf("G.C.D of %d and %d is %d", n1, n2, gcd);
+
     return 0;
 }

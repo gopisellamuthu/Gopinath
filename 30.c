503b126e5a1cb9bb4d8d86104d5aa599721a7541
gopisellamuthu <30342205+gopisellamuthu@users.noreply.github.com>
2017-08-16 06:06:46 +0530

    Create 30.c

---
+++ 30.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, a, sum=0;
+    printf("Enter upper limit: ");
+    scanf("%d", &a);
+    for(i=1; i<=n; i++)
+    {
+        sum += i;
+    }
+
+    printf("Sum of first %d natural numbers = %d", a, sum);
+
+    return 0;
+}
